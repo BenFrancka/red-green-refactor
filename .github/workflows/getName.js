70ae884297775d552498ed05c982089706f150c0
BenFrancka <benfrancka@gmail.com>
2021-07-19 16:59:11 -0700

    adds refactored getName function, with object destructuring

--- .github/workflows/getName.js
+++ .github/workflows/getName.js
@@ -1,6 +1,6 @@
 //function should take in an object and return its name value
-const getName = (obj) => {
-    return obj.name;
+const getName = ({ name }) => {
+    return name;
 }
 
 export default getName;
