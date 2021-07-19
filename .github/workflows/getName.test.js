549e0bde6bacda0d70c1c047784eca9a72dbcdff
BenFrancka <benfrancka@gmail.com>
2021-07-19 16:56:30 -0700

    adds second passing test for getName function with character object

--- .github/workflows/getName.test.js
+++ .github/workflows/getName.test.js
@@ -8,4 +8,13 @@ describe('getName', () => {
 
         expect(actual).toEqual(expected);
     });
+
+    it('returns the name property of an object', () => {
+        const character = { _id: '5cf5679a915ecad153ab68c9', name: 'Aang' }
+        const expected = character.name; 
+        const actual = getName(character);
+
+        expect(actual).toEqual(expected);
+    });
+    
 });
