abd5fa8f7d29fc7ac636942c0ba22ac3e8908021
BenFrancka <benfrancka@gmail.com>
2021-07-20 09:31:37 -0700

    refactors copy and push function

--- .github/workflows/copyAndPush.js
+++ .github/workflows/copyAndPush.js
@@ -1,10 +1,6 @@
 //function returns new array with all items from original array and a new item pushed to the end
 const copyAndPush = (someArray, newItem) => {
-    const newArray=  someArray;
-    const newArrayCopy= [...newArray];
-
-    newArrayCopy.push(newItem);
-    return newArrayCopy;
+    return [...someArray, newItem]
 }
 
 export default copyAndPush;
