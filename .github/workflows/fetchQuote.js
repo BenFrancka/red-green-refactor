ffa0885adc24005ca9dd8854a4308c8022b9a304
BenFrancka <benfrancka@gmail.com>
2021-07-19 19:26:13 -0700

    adds passing test for fetchQuote function

---
+++ .github/workflows/fetchQuote.js
@@ -0,0 +1,10 @@
+import fetch from 'node-fetch';
+
+const fetchQuote = async () => {
+    const res = await fetch('https://futuramaapi.herokuapp.com/api/quotes');
+    const body = await res.json();
+
+    return body[1];
+};
+
+export default fetchQuote;
