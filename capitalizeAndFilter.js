c02cf732b3fc1d7c2f48eb523e235675fafc1301
BenFrancka <benfrancka@gmail.com>
2021-07-21 13:33:30 -0700

    attempts to add for loop to capitalize and filter, still not working

--- capitalizeAndFilter.js
+++ capitalizeAndFilter.js
@@ -1,6 +1,10 @@
 //takes in an array of strings, capitalizes all the strings, and filters out any string that starts with F/f.
 
 const capitalizeAndFilter = (stringArray) => {
+  // for(let index = 0; index < stringArray.length; index++) {
+  //   const element = stringArray[index];
+  //   element.toUpperCase();
+  // }
   const upperCaseArray = stringArray.map(string => string.toUpperCase());
 
   const filteredArray = upperCaseArray.filter(string => string[0] !== 'F');
