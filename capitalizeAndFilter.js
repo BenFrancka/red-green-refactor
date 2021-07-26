09cc3dc6ce858e8ceef074aadce0e2542965bf9d
BenFrancka <benfrancka@gmail.com>
2021-07-26 11:23:31 -0700

    adds back refactored capitolize and filter with array methods

--- capitalizeAndFilter.js
+++ capitalizeAndFilter.js
@@ -1,23 +1,16 @@
 //takes in an array of strings, capitalizes all the strings, and filters out any string that starts with F/f.
 
 const capitalizeAndFilter = (stringArray) => {
-  const newArray = [];
+  const upperCaseArray = stringArray.map(string => string.toUpperCase());
 
-  for(const string of stringArray) {
-    if(string[0] !== 'f' && string[0] !== 'F') {
-      newArray.push(string.toUpperCase());
-    }
-  }
-  return newArray;
+  const filteredArray = upperCaseArray.filter(string => string[0] !== 'F');
+
+  return filteredArray;
 };
 
   
 
-//const upperCaseArray = stringArray.map(string => string.toUpperCase());
 
-//const filteredArray = upperCaseArray.filter(string => string[0] !== 'F');
-
-//return filteredArray;
 
 
 export default capitalizeAndFilter;
